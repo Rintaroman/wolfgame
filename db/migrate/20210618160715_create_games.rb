f43e3e058dfd9304a479d6c1d695a2314b87aaef
Rintaroman <rintaro.584@gmail.com>
2021-06-19 01:13:01 +0900

    データベース作成

--- db/migrate/20210618160715_create_games.rb
+++ db/migrate/20210618160715_create_games.rb
@@ -1,7 +1,8 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
-
+      t.references :player,          foreign_key: true
+      t.references :job,          foreign_key: true
       t.timestamps
     end
   end
