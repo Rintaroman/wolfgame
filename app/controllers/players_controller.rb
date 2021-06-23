a4ed5c43361773abe387d3f0d64babd89cc9a84f
Rintaroman <rintaro.584@gmail.com>
2021-06-23 16:41:14 +0900

    新規プレイヤー作成機能

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,17 @@
 class PlayersController < ApplicationController
+  def new
+    @player = Player.new    
+  end
+
+  def create
+    @player = Player.new(player_params)
+    @player.save
+    redirect_to root_path
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name)
+  end
 end
