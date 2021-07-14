c911f2f42d8e407966130130dead71d7ffcb0e95
Rintaroman <rintaro.584@gmail.com>
2021-07-14 13:10:43 +0900

    役職カウント表示

--- app/javascript/join.js
+++ app/javascript/join.js
@@ -4,5 +4,7 @@ window.addEventListener('load', ()=>{
     const inputValue = joinSelect.value;
     const joinMember = document.getElementById("join-list");
     joinMember.innerHTML =inputValue + "人のプレイヤーを選択してください"
+    const joinJob = document.getElementById("join-job")
+    joinJob.innerHTML = parseInt(inputValue) + 2 + "つの役職を選択してください"
   })
 }) 
